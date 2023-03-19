e221a3d85116f4e5cca7407b44686dc12b405c78
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-03-19 15:10:33 -0400

    adds automatic theme

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-function lightmode() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-    if (element.classList.contains("light-mode")) {
-        document.getElementById("screen-mode").src="./img/switch-on.png"
-    } else {
-        document.getElementById("screen-mode").src="./img/switch-off.png"
-    }
+function theme() {
+  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    var body = document.body;
+    body.classList.toggle("light-mode");
+    document.getElementById("screen-mode").src = "./img/switch-on.png";
+  }
 }
+
+function lightmode() {
+  var body = document.body;
+  body.classList.toggle("light-mode");
+  if (body.classList.contains("light-mode")) {
+    document.getElementById("screen-mode").src = "./img/switch-on.png";
+  } else {
+    document.getElementById("screen-mode").src = "./img/switch-off.png";
+  }
+}
+
+theme();
